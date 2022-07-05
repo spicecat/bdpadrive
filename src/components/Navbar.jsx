e4b932311e064dfb3e575e498c6d8100f6963d22
“spicecat” <ateh0701@gmail.com>
2022-07-05 18:27:38 -0500

    navbar mui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,23 @@
 import { Link } from 'react-router-dom'
-// import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core'
-// import HomeIcon from '@material-ui/icons/Home'
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material'
+// import HomeIcon from '@mui/icons-material/Home'
 
 export default function Navbar() {
 
     return (
-        // <AppBar position='static'>
-        //     <Toolbar variant='dense'>
-        //         <IconButton color='inherit' className='menu-button' component={Link} to='/' >
-        //             <HomeIcon />
-        //         </IconButton>
-        //         &nbsp;
-        //         <Typography>TODO: page</Typography>
-        //         <>
-        //             <Button color='inherit' variant='outlined' size='small' to='/' component={Link}>Home</Button>&nbsp;
-        //             <Button color='inherit' variant='outlined' size='small' to='/explorer' component={Link}>Explorer</Button>&nbsp;
-        //             <Button color='inherit' variant='outlined' size='small' to='/login' component={Link}>Login</Button>&nbsp;
-        //         </>
-        //     </Toolbar>
-        // </AppBar >
-        <div>navbar</div>
+        <AppBar position='static'>
+            <Toolbar variant='dense'>
+                <IconButton color='inherit' className='menu-button' component={Link} to='/' >
+                    {/* <HomeIcon /> */}
+                </IconButton>
+                &nbsp;
+                <Typography>TODO: page</Typography>
+                <>
+                    <Button color='inherit' variant='outlined' size='small' to='/' component={Link}>Home</Button>&nbsp;
+                    <Button color='inherit' variant='outlined' size='small' to='/explorer' component={Link}>Explorer</Button>&nbsp;
+                    <Button color='inherit' variant='outlined' size='small' to='/login' component={Link}>Login</Button>&nbsp;
+                </>
+            </Toolbar>
+        </AppBar>
     )
 }
