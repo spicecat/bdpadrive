6c585b23aa9bc5683ab3aa70a80af06a82e8f57e
“spicecat” <ateh0701@gmail.com>
2022-07-05 17:39:04 -0500

    navbar template

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core'
+import HomeIcon from '@material-ui/icons/Home'
+
+export default function Navbar() {
+
+    return (
+        <AppBar position='static'>
+            <Toolbar variant='dense'>
+                <IconButton color='inherit' className='menu-button' component={Link} to='/' >
+                    <HomeIcon />
+                </IconButton>
+                &nbsp;
+                <Typography>TODO: page</Typography>
+                <>
+                    <Button color='inherit' variant='outlined' size='small' to='/explorer' component={Link}>Explorer</Button>&nbsp;
+                </>
+            </Toolbar>
+        </AppBar >
+
+    )
+}
