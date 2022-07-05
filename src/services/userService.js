d5e3410290c6f5bc97bd762ff39719dec9f97cba
“spicecat” <ateh0701@gmail.com>
2022-07-05 17:46:32 -0500

    var.js constants

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,7 +2,7 @@ import superagent from 'superagent'
 import superagentCache from 'superagent-cache'
 import Cookies from 'universal-cookie'
 
-import { SERVER, API_KEY } from '../var.js'
+import { API, SERVER, API_KEY } from '../var.js'
 
 superagentCache(superagent, null, { preventDuplicateCalls: true })
 const cookies = new Cookies()
